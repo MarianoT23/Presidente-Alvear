b72e86d1b4eca1bc109e3f44e961a7db24f841fb
Mariano Timistit <mariano@wcanvas.com>
2021-01-17 17:26:48 -0300

    Styles

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
-// Navbar active on scroll
-window.addEventListener('scroll', () => {
-    console.log(window.scrollTop)
+console.time('JS is loading fine')
+
+// Navbar Active
+document.addEventListener('scroll', function() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.querySelector('.js-nav').classList.add('active')
+    } else {
+        document.querySelector('.js-nav').classList.remove('active')
+    }
 })
+
+// Hamburger Menu
+const hamburgerMenuContainer = document.getElementById
+('js-hamburger-menu-container')
+const hamburgerMenuLines = document.getElementById('js-hamburger-menu-lines')
+const navbarMenuContainer = document.getElementById('js-navbar-menu-container')
+
+hamburgerMenuContainer.addEventListener('click', () => {
+    if (hamburgerMenuLines.classList.contains('open')) {
+        hamburgerMenuLines.classList.remove('open')
+        navbarMenuContainer.classList.remove('active')
+    } else {
+        hamburgerMenuLines.classList.add('open')
+        navbarMenuContainer.classList.add('active')
+    }
+})
+
+console.timeEnd("JS is loading fine");
