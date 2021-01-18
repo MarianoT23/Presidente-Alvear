5783b9b8df1e33397bc5a041a1e6ac04501812e9
Mariano Timistit <mariano@wcanvas.com>
2021-01-18 20:23:53 -0300

    JS

--- main.js
+++ main.js
@@ -1,39 +1,35 @@
-window.addEventListener("DOMContentLoaded", function() {
+console.time('JS is loading fine')
 
-    console.time('JS is loading fine')
-
-    // Navbar Active
-    document.addEventListener('scroll', function() {
-        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-            document.querySelector('.js-nav').classList.add('active')
-        } else {
-            document.querySelector('.js-nav').classList.remove('active')
-        }
-    })
-
-    // Hamburger Menu
-    const hamburgerMenuContainer = document.getElementById
-    ('js-hamburger-menu-container')
-    const hamburgerMenuLines = document.getElementById('js-hamburger-menu-lines')
-    const navbarMenuContainer = document.getElementById('js-navbar-menu-container')
-    const menuListItems = document.querySelectorAll('#js-navbar-menu-container li')
-
-    hamburgerMenuContainer.addEventListener('click', () => {
-        if (hamburgerMenuLines.classList.contains('open')) {
-            hamburgerMenuLines.classList.remove('open')
-            navbarMenuContainer.classList.remove('active')
-        } else {
-            hamburgerMenuLines.classList.add('open')
-            navbarMenuContainer.classList.add('active')
-        }
-    })
-    menuListItems.forEach(listItem => {
-        listItem.addEventListener('click', () => {
-            hamburgerMenuLines.classList.remove('open')
-            navbarMenuContainer.classList.remove('active')
-        })
-    });
-
-    console.timeEnd("JS is loading fine");
-
+// Navbar Active
+document.addEventListener('scroll', function() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.querySelector('.js-nav').classList.add('active')
+    } else {
+        document.querySelector('.js-nav').classList.remove('active')
+    }
 })
+
+// Hamburger Menu
+const hamburgerMenuContainer = document.getElementById
+('js-hamburger-menu-container')
+const hamburgerMenuLines = document.getElementById('js-hamburger-menu-lines')
+const navbarMenuContainer = document.getElementById('js-navbar-menu-container')
+const menuListItems = document.querySelectorAll('#js-navbar-menu-container li')
+
+hamburgerMenuContainer.addEventListener('click', () => {
+    if (hamburgerMenuLines.classList.contains('open')) {
+        hamburgerMenuLines.classList.remove('open')
+        navbarMenuContainer.classList.remove('active')
+    } else {
+        hamburgerMenuLines.classList.add('open')
+        navbarMenuContainer.classList.add('active')
+    }
+})
+menuListItems.forEach(listItem => {
+    listItem.addEventListener('click', () => {
+        hamburgerMenuLines.classList.remove('open')
+        navbarMenuContainer.classList.remove('active')
+    })
+});
+
+console.timeEnd("JS is loading fine");
