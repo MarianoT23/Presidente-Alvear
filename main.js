4ae9a6597ae8e11fbe2c8b7341cebf60c240ef1e
Mariano Timistit <mariano@wcanvas.com>
2021-02-08 20:38:56 -0300

    Added New Documentos Historicos

--- main.js
+++ main.js
@@ -56,7 +56,59 @@ window.addEventListener('load', function(){
         })
     });
   
+    // Documentos Históricos (Filter)
+    const filterTodos = document.getElementById('js-documento-filter--todos')
+    const filterCartas = document.getElementById('js-documento-filter--cartas')
+    const filterPrensa = document.getElementById('js-documento-filter--prensa')
+    const documentosHistoricosTodos = document.querySelectorAll('.documentos-historicos__documento-container')
+    const documentosHistoricosCartas = document.querySelectorAll('.js-carta')
+    const documentosHistoricosPrensa = document.querySelectorAll('.js-prensa')
+    const documentosHistoricosContainer = document.getElementById('js-documentos-historicos')
+
+    if (documentosHistoricosContainer) {
+        filterTodos.classList.add('active')
+        documentosHistoricosTodos.forEach(element => {
+            element.classList.add('active')
+        });
     
+        filterTodos.addEventListener('click', () => {
+            filterTodos.classList.add('active')
+            filterCartas.classList.remove('active')
+            filterPrensa.classList.remove('active')
+            documentosHistoricosCartas.forEach(element => {
+                element.classList.add('active')
+            });
+            documentosHistoricosPrensa.forEach(element => {
+                element.classList.add('active')
+            })
+        })
+    
+        filterCartas.addEventListener('click', () => {
+            filterTodos.classList.remove('active')
+            filterCartas.classList.add('active')
+            filterPrensa.classList.remove('active')
+            documentosHistoricosCartas.forEach(element => {
+                element.classList.add('active')
+            });
+            documentosHistoricosPrensa.forEach(element => {
+                element.classList.remove('active')
+            })
+        })
+    
+        filterPrensa.addEventListener('click', () => {
+            filterTodos.classList.remove('active')
+            filterCartas.classList.remove('active')
+            filterPrensa.classList.add('active')
+            documentosHistoricosPrensa.forEach(element => {
+                element.classList.add('active')
+            });
+            documentosHistoricosCartas.forEach(element => {
+                element.classList.remove('active')
+            })
+        })
+    }
+
+
     console.timeEnd("JS is loading fine");
 
 })
